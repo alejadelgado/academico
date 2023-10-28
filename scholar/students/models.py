a3fb8bd09f009659d073d3f55ba530e3dc46fb02
claudia <alejaj3399@gmail.com>
2023-10-27 20:02:19 -0500

    claseaula4

--- scholar/students/models.py
+++ scholar/students/models.py
@@ -3,4 +3,6 @@ from django.db import models
 class User(models.Model):
     email=models.CharField(max_length=100)
     password=models.CharField(max_length=500)
+    dni=models.CharField(max_length=10,default="")
+    age=models.IntegerField(default=0)
 # Create your models here.
